5101231e8b4a91b44f82c5324ae80d57fb74fca3
beanchhoryi <beanchhoryi@gmail.com>
2025-12-25 01:52:53 +0700

    fix 014

--- app_school/admin.py
+++ app_school/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 
 from django.contrib import admin
 from .models import Student
+from .models import Teacher
+from .models import Subject
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'gender', 'dob']
+    list_display = ['first_name', 'last_name', 'gender', 'dob', 'address']
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'gender', 'dob', 'address', 'salary']
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['subject_name']
+
