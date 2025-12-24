769a21ee37620ba3c61896d1eca3860a212011fa
beanchhoryi <beanchhoryi@gmail.com>
2025-12-25 00:40:24 +0700

    fix 013

--- school_management/settings.py
+++ school_management/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_school',
-    'app_school.apps.AppSchoolConfig',
+    # 'app_school.apps.AppSchoolConfig',
 ]
 
 MIDDLEWARE = [
